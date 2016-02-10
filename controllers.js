cfaad6fa8aa6d0a26b430cdb69492da20232d62a
jroers <joe.roers@gmail.com>
2016-02-10 15:06:08 -0800

    Got rid of stupid semicolons. still, like a bau5

--- controllers.js
+++ controllers.js
@@ -12,19 +12,19 @@ function BooksController (Book) {
   function updateBook(book) {
     Book.update({id: book._id}, book);
     book.editForm = false;
-  };
+  }
 
   function createBook(){
     Book.save(this.newBook);
     // TODO: clear the form!
     // TODO: display the new book in the list of books!
-  };
+  }
 
   function deleteBook(book) {
     Book.remove({id:book._id});
     var bookIndex = this.books.indexOf(book);
     this.books.splice(bookIndex, 1);
-  };
+  }
 
   console.log("Controller loaded.");
-};
+}
